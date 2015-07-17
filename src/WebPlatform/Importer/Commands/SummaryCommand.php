c52468367ddbb9dcd492765c325afe3096152b5b
Renoir Boulanger <renoir@w3.org>
2015-07-17 15:11:55 -0400

    Commit message error while attempting to set revision id

--- src/WebPlatform/Importer/Commands/SummaryCommand.php
+++ src/WebPlatform/Importer/Commands/SummaryCommand.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Input\InputOption;
 
 use WebPlatform\ContentConverter\Model\MediaWikiDocument;
 use WebPlatform\ContentConverter\Model\MediaWikiContributor;
@@ -42,8 +43,14 @@ class SummaryCommand extends Command
                 to store on a filesystem.
 DESCR;
         $this
-            ->setName('mediawiki:summary')
-            ->setDescription($description);
+            ->setName('mediawiki:run')
+            ->setDescription($description)
+            ->setDefinition(
+                [
+                    new InputOption('git', '', InputOption::VALUE_NONE, 'Whether or not to convert into a git repository ', null),
+                ]
+            );
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -51,14 +58,7 @@ DESCR;
         $header_style = new OutputFormatterStyle('white', 'black', array('bold'));
         $output->getFormatter()->setStyle('header', $header_style);
 
-        $this->filesystem = new Filesystem;
-
-        $repoInitialized = (realpath(GIT_OUTPUT_DIR.'/.git') === false)?false:true;
-        //die(var_dump(array($repoInitialized, realpath(GIT_OUTPUT_DIR))));
-        $this->git = new GitRepository(realpath(GIT_OUTPUT_DIR));
-        if ($repoInitialized === false) {
-            $this->git->init()->execute();
-        }
+        $useGit = $input->getOption('git');
 
         $moreThanHundredRevs = [];
         $translations = [];
@@ -77,7 +77,18 @@ DESCR;
 
         $counter = 1;
         $maxHops = 0;
-        $revMaxHops = 0;
+        $revMaxHops = 10;
+
+        if ($useGit === true) {
+            $this->filesystem = new Filesystem;
+
+            $repoInitialized = (realpath(GIT_OUTPUT_DIR.'/.git') === false)?false:true;
+            //die(var_dump(array($repoInitialized, realpath(GIT_OUTPUT_DIR))));
+            $this->git = new GitRepository(realpath(GIT_OUTPUT_DIR));
+            if ($repoInitialized === false) {
+                $this->git->init()->execute();
+            }
+        }
 
         /**
          * Author array of MediaWikiContributor objects with $this->users[$uid],
@@ -132,7 +143,7 @@ DESCR;
                 /** ----------- REVISION --------------- */
                 for ($revisionsList->rewind(); $revisionsList->valid(); $revisionsList->next()) {
                     if ($revMaxHops > 0 && $revMaxHops === $revCounter) {
-                        $output->writeln(sprintf('Reached desired maximum of %d revision loops', $revMaxHops).PHP_EOL.PHP_EOL);
+                        $output->writeln(sprintf('    - stop: Reached maximum %d revision loops', $revMaxHops).PHP_EOL.PHP_EOL);
                         break;
                     }
 
@@ -153,50 +164,55 @@ DESCR;
                         // Hopefully we won’t get any here!
                         $problematic_author_entry[] = sprintf('{revision_id: %d, contributor_id: %d}', $revision_id, $contributor_id);
                     }
+
                     $author = $wikiRevision->getAuthor();
                     $contributor_name = (is_object($author))?$author->getRealName():'No real name set';
-
+                    $author_string = (string) $author;
+                    $author_string = (empty($author_string))?"Anonymous Contributor <public-webplatform@w3.org>":$author_string;
                     $timestamp = $wikiRevision->getTimestamp()->format(DateTime::RFC2822);
+                    $comment = $wikiRevision->getComment();
+                    $comment_shorter = mb_strimwidth($comment, strpos($comment, ': ') + 2, 100);
 
                     $persistable->setRevision($wikiRevision);
-                    $commit_commands = $persistable->formatPersisterCommand();
-                    $commit_args = $persistable->getArgs();
 
-                    $this->filesystem->dumpFile($persistable->getName(), (string) $persistable);
+                    if ($useGit === true) {
+                        $this->filesystem->dumpFile($persistable->getName(), (string) $persistable);
+                    }
 
                     $output->writeln(sprintf('    - id: %d', $revision_id));
                     $output->writeln(sprintf('      timestamp: %s', $timestamp));
                     $output->writeln(sprintf('      full_name: %s', $contributor_name));
-                    $output->writeln(sprintf('      author: %s', (string) $author));
-                    $output->writeln(sprintf('      user_id: %d', $contributor_id));
-                    $output->writeln(sprintf('      commit: %s', join(' ; ', $commit_commands)));
+                    $output->writeln(sprintf('      author: %s', $author_string));
+                    $output->writeln(sprintf('      comment: %s', $comment_shorter));
 
-                    try {
-                        $this->git
-                            // Make sure out/ matches what we set at GitCommitFileRevision constructor.
-                            ->add()
-                            ->execute(preg_replace('/^out\//', '', $persistable->getName()));
-                    } catch (GitException $e) {
-                        $message = sprintf('Could not add file %s for revision %d', $persistable->getName(), $revision_id);
-                        throw new Exception($message, null, $e);
+                    if ($useGit === true) {
+                        try {
+                            $this->git
+                                ->add()
+                                // Make sure out/ matches what we set at GitCommitFileRevision constructor.
+                                ->execute(preg_replace('/^out\//', '', $persistable->getName()));
+                        } catch (GitException $e) {
+                            $message = sprintf('Could not add file %s for revision %d', $persistable->getName(), $revision_id);
+                            throw new Exception($message, null, $e);
+                        }
+
+                        try {
+                            $this->git
+                                ->commit()
+                                ->message('"'.$comment().'"')
+                                ->author('"'.$author_string.'"')
+                                ->date('"'.$timestamp.'"')
+                                ->allowEmpty()
+                                ->execute();
+
+                        } catch (GitException $e) {
+                            var_dump($this->git);
+                            $message = sprintf('Could not commit for revision %d', $revision_id);
+                            throw new Exception($message, null, $e);
+                        }
+
+                        //die(var_dump($this->git));
                     }
-
-                    try {
-                        $this->git
-                            ->commit()
-                            ->message('"'.$wikiRevision->getComment().'"')
-                            ->author('"'.(string) $author.'"')
-                            ->date('"'.$timestamp.'"')
-                            ->allowEmpty()
-                            ->execute();
-
-                    } catch (GitException $e) {
-                        var_dump($this->git);
-                        $message = sprintf('Could not commit for revision %d', $revision_id);
-                        throw new Exception($message, null, $e);
-                    }
-
-                    //die(var_dump($this->git));
 
                     ++$revCounter;
                 }
