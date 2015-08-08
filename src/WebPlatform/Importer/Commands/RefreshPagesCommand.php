763102a534eac5967d50d4c29c42ab6f69e675fd
Renoir Boulanger <renoir@w3.org>
2015-08-08 14:58:34 -0400

    Mistake in the wiki page to send a ?action=purge to

--- src/WebPlatform/Importer/Commands/RefreshPagesCommand.php
+++ src/WebPlatform/Importer/Commands/RefreshPagesCommand.php
@@ -9,9 +9,13 @@ namespace WebPlatform\Importer\Commands;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Yaml;
+use Prewk\XmlStringStreamer;
+use WebPlatform\Importer\Model\MediaWikiDocument;
 use WebPlatform\ContentConverter\Converter\MediaWikiToHtml;
+use SimpleXMLElement;
 use Exception;
 
 /**
@@ -70,24 +74,29 @@ DESCR;
             ->setDescription($description)
             ->setDefinition(
                 [
-                    // Nothing!
+                    new InputOption('xml-source', '', InputOption::VALUE_OPTIONAL, 'What file to read from. Argument is relative from data/ folder from this directory (e.g. foo.xml in data/foo.xml)', 'dumps/main_full.xml'),
                 ]
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $xmlSource = $input->getOption('xml-source');
+
         $missed_file = DATA_DIR.'/missed.yml';
         if (realpath($missed_file) === false) {
             throw new Exception(sprintf('Could not find missed file at %s', $missed_file));
         }
+        
         $missedFileContents = file_get_contents($missed_file);
         $parser = new Yaml\Parser();
+        
         try {
             $missed = $parser->parse($missedFileContents);
         } catch (Exception $e) {
             throw new Exception(sprintf('Could not get file %s contents to be parsed as YAML. Is it in YAML format?', $missed_file), null, $e);
         }
+
         if (!isset($missed['missed'])) {
             throw new Exception('Please ensure missed.yml has a list of titles under a "missed:" top level key');
         }
@@ -118,19 +127,40 @@ DESCR;
 
         $output->writeln('Sending purge:');
 
-        foreach ($missed['missed'] as $title) {
-            try {
-                $purgeCall = $apiManager->makeRequest($title, $cookieString);
-            } catch (Exception $e) {
-                $message = 'Had issue with attempt to refresh page from MediaWiki for %s';
-                throw new Exception(sprintf($message, $title), 0, $e);
-            }
-            if (empty($purgeCall)) {
-                $message  = 'Refresh call did not work, we expected a HTML and got nothing, check at %s%s gives from a web browser';
-                throw new Exception(sprintf($message, $apiUrl, $title));
-            }
+        /* -------------------- XML source -------------------- **/
+        $file = realpath(DATA_DIR.'/'.$xmlSource);
+        if ($file === false) {
+            throw new Exception(sprintf('Cannot run script, source XML file ./data/%s could not be found', $xmlSource));
+        }
+        $streamer = XmlStringStreamer::createStringWalkerParser($file);
+        /* -------------------- /XML source -------------------- **/
 
-            $output->writeln(' - '.$title);
+        while ($node = $streamer->getNode()) {
+
+            $pageNode = new SimpleXMLElement($node);
+            if (isset($pageNode->title)) {
+                $wikiDocument = new MediaWikiDocument($pageNode);
+
+                $title = $wikiDocument->getTitle();
+                $normalized_location = $wikiDocument->getName();
+
+                if (!in_array($normalized_location, $missed['missed'])) {
+                    continue;
+                }
+
+                try {
+                    $purgeCall = $apiManager->makeRequest($title, $cookieString);
+                } catch (Exception $e) {
+                    $message = 'Had issue with attempt to refresh page from MediaWiki for %s';
+                    throw new Exception(sprintf($message, $title), 0, $e);
+                }
+                if (empty($purgeCall)) {
+                    $message  = 'Refresh call did not work, we expected a HTML and got nothing, check at %s%s gives from a web browser';
+                    throw new Exception(sprintf($message, $apiUrl, $title));
+                }
+
+                $output->writeln(' - '.$title);
+            }
         }
     }
 }
